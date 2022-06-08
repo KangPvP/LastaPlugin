fa1d3bf765bd7e375e01b555f3b7141ae3f6bc1d
KangPvP <82943439+KangPvP@users.noreply.github.com>
2022-06-08 18:57:53 +0200

    Updata 0.00000001

--- src/main/java/fr/kangpvp/lastarria/commands/CommandBoutique.java
+++ src/main/java/fr/kangpvp/lastarria/commands/CommandBoutique.java
@@ -1,5 +1,6 @@
 package fr.kangpvp.lastarria.commands;
 
+import fr.kangpvp.lastarria.utils.Grade;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +14,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class CommandBoutique implements CommandExecutor {
@@ -39,21 +43,64 @@ public class CommandBoutique implements CommandExecutor {
                     cle.setItemMeta(cleM);
                     InvGradeData.addItem(cle);
 
-                    ItemStack Vilain = InvGradeData.getItem(2); //Villain
-                    ItemStack Paysan = InvGradeData.getItem(3); //Paysan
-                    ItemStack Artisans = InvGradeData.getItem(4); //Artisans
-                    ItemStack Ecuyer = InvGradeData.getItem(5); //Artisans
+                    ItemStack Vilain = InvGradeData.getItem(0); //Vilain
+
+                    ItemMeta VilainMeta = Vilain.getItemMeta();
+                    VilainMeta.setDisplayName("§6Cle");
+
+                    Vilain.setItemMeta(cleM);
+
+
+                    ItemStack Paysan = InvGradeData.getItem(1); //Paysan
+                    List<String> listPaysan = new ArrayList<String>();
+                    listPaysan.add("3 items en vente");
+                    listPaysan.add("2 homes");
+
+                    ItemStack Commercant = InvGradeData.getItem(2); //Commercant
+                    List<String> listCommercant = new ArrayList<String>();
+                    listCommercant.add("4 items en vente");
+                    listCommercant.add("ATM 1.5$/min");
+
+                    ItemStack Artisan = InvGradeData.getItem(3); //Artisans
+
+
+                    ItemStack Ecuyer = InvGradeData.getItem(4); //Ecuyer
+                    ItemStack Vassal = InvGradeData.getItem(5); //Vassal
+                    ItemStack Chevalier = InvGradeData.getItem(6); //Chevalier
+                    ItemStack Chatelain = InvGradeData.getItem(7); //Chatelain
+                    ItemStack Baron = InvGradeData.getItem(8); //Baron
+                    ItemStack Comte = InvGradeData.getItem(9); //Comte
+                    ItemStack Duc = InvGradeData.getItem(10); //Duc
+                    ItemStack Seigneur = InvGradeData.getItem(11); //Seigneur
+
+
+                    Grade grade = new Grade(Paysan, "Paysan", "§2", 0, 300, null, listPaysan);
+                    Paysan.setItemMeta(grade.getMeta());
+
+
 
                     Inventory inv = Bukkit.createInventory(null, 54, "Boutique");
 
-                    inv.setItem(1, Vilain);
+                   // inv.setItem(11, Vilain);
+                    inv.setItem(12, Paysan);
+                    /*inv.setItem(13, Commercant);
+                    inv.setItem(14, Artisan);
+                    inv.setItem(15, Ecuyer);
+                    inv.setItem(20, Vassal);
+                    inv.setItem(21, Chevalier);
+                    inv.setItem(22, Chatelain);
+                    inv.setItem(30, Baron);
+                    inv.setItem(31, Comte);
+                    inv.setItem(32, Duc);
+                    inv.setItem(40, Seigneur);*/
+
 
                     inv.setItem(2, myAwesomeSkull);
                     inv.setItem(1, Vilain);
                     inv.setItem(0, new ItemStack(Material.NETHERITE_SWORD));
                     player.openInventory(inv);
                 }else {
-                    player.sendMessage("Erreur: Les datas de cette commande ne sont pas chargé\nMerci de contacter un Administrateur pour régler ce bug");
+                    player.sendMessage("Erreur: Les datas de cette commande ne sont pas chargé\nMerci de contacter KangPvP pour régler ce bug");
                 }
             }
 
---
+++ src/main/java/fr/kangpvp/lastarria/utils/Grade.java
@@ -0,0 +1,80 @@
+package fr.kangpvp.lastarria.utils;
+
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.persistence.PersistentDataContainer;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Grade {
+
+    private ItemStack item;
+    private String name;
+    private String color;
+    private List<String> content;
+    private String previousName;
+
+    public Grade(ItemStack item, String name, String color, int hour, int price, Grade previousGrade, List<String> avantages) {
+
+        this.item = item;
+
+        this.name = "§" + color + "§l" + name;
+
+        if(previousGrade == null){
+            this.previousName = "Aucun";
+
+        } else {
+            this.previousName = previousGrade.getName();
+        }
+
+        this.content = Lists.newArrayList(
+                "",
+                "§c§nConditions",
+                "§7 • Titre: " + this.previousName,
+                "§7 • " + hour + " heures de jeu",
+                "§7 • prix",
+                "",
+                "§a§nAvantages"
+        );
+
+        avantages.forEach((avantage) -> {
+            avantage = "§7 • " + avantage;
+            this.content.add(avantage);
+        });
+
+        this.content.add("");
+        this.content.add("§eClic gauche §fpour acheter le grade " + this.name);
+
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<String> getContent() {
+        return this.content;
+    }
+
+    public ItemMeta getMeta() {
+
+        ItemMeta meta = this.item.getItemMeta();
+
+        meta.setDisplayName(this.name);
+        meta.setLore(this.content);
+        return meta;
+
+    }
+}
