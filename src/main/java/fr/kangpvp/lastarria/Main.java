bdeeb0ab0a8c2bbff942bb1fbf665d471f9686d3
KangPvP <82943439+KangPvP@users.noreply.github.com>
2022-06-17 23:14:17 +0200

    Updata KangPvP 16/06

--- src/main/java/fr/kangpvp/lastarria/Main.java
+++ src/main/java/fr/kangpvp/lastarria/Main.java
@@ -1,6 +1,5 @@
 package fr.kangpvp.lastarria;
 
-import com.google.common.collect.Lists;
 import fr.kangpvp.lastarria.commands.*;
 import fr.kangpvp.lastarria.commands.lastacoin.CommandLastacoin;
 import fr.kangpvp.lastarria.listener.InteractListener;
@@ -8,12 +7,8 @@ import fr.kangpvp.lastarria.listener.PlayerListener;
 import fr.kangpvp.lastarria.utils.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 
 public final class Main extends JavaPlugin {
---
+++ src/main/java/fr/kangpvp/lastarria/commands/CommandFly.java
@@ -0,0 +1,50 @@
+package fr.kangpvp.lastarria.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandFly implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+
+                if(sender instanceof Player) {
+                    System.out.println("test perm");
+                    Player player = (Player) sender;
+                    if(player.getAllowFlight()) {
+                        player.sendMessage("§bVous pouvez maintenant voler.");
+                    } else {
+                        player.sendMessage("§bVous ne pouvez maintenant plus voler.");
+                    }
+                    player.setAllowFlight(!player.getAllowFlight());
+                    player.setFlySpeed(0.06F);
+                } else {
+                    System.out.println("test pas la perm");
+                    sender.sendMessage("§cVous n'avez pas la permission requise.");
+                }
+                System.out.println("test final");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return false;
+    }
+}
